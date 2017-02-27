207e4f6ba709bf2ab3156aecf7606b427594b485
Kees Hink <keeshink@gmail.com>
2017-02-27 18:29:26 +0100

    Fix the sorted storing rewrite.

--- tk17_tg_bot/result.py
+++ tk17_tg_bot/result.py
@@ -5,13 +5,15 @@ class Result(object):
     """Class for formatting raw votes data."""
 
     def __init__(self, votes):
-        """Store raw votes by popularity."""
-        self.votes = sorted(votes,
-                            key=self.votes.get,
-                            reverse=True)
+        """Store raw votes."""
+        self.votes = votes
 
     def __str__(self):
         """Render as string."""
+        if not self.votes:
+            return "Er is nog niet gestemd."
         return '\n'.join(
             ["%s: %s" % (key, value)
-             for key, value in self.sorted_votes.iteritems()])
+             for key, value in sorted(self.votes.iteritems(),
+                                      key=self.votes.get,
+                                      reverse=True)])
