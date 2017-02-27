2f14306f5df01bbf042d945151bdc6f080af7ace
Kees Hink <keeshink@gmail.com>
2017-02-27 23:59:55 +0100

    Refactor a bit for readability.

--- tk17_tg_bot/result.py
+++ tk17_tg_bot/result.py
@@ -5,14 +5,15 @@ class Result(object):
     """Class for formatting raw votes data."""
 
     def __init__(self, votes):
-        """Store raw votes."""
+        """Store votes."""
+        # raw votes
         self.votes = votes
+        # list of options in decreasing popularity
+        self.options = sorted(votes, key=self.votes.get, reverse=True)
 
     def __str__(self):
         """Render as string."""
         if not self.votes:
             return "Er is nog niet gestemd."
         return '\n'.join(["%s: %s" % (key, self.votes[key])
-                          for key in sorted(self.votes,
-                                            key=self.votes.get,
-                                            reverse=True)])
+                          for key in self.options])
